fdd4eacf80c83caafc4ec527b3df9fa54e70609f
pan <panxiaozhong@gmail.com>
2021-12-20 15:18:10 -0800

    fix link get removed when evernote re-render some nodes

--- addon/content_scripts/content.js
+++ addon/content_scripts/content.js
@@ -2,12 +2,21 @@
 
   class EverNoteHelper {
     constructor() {
+      this.articleContainerSelector = '#qa-NOTES_SIDEBAR .rv-sticky-leaf-node';
+      // set this attr to note id if possible, set to "null" when the container
+      // is not a note container
       this.attrName = "data-nierevernote-id";
+      // set this attr to generated links
       this.linkAttrName = "data-nierevernote-link";
       this.attrValueNull = "null";
       this.debug = false;
       this.helperLinkJobInterval = null;
       this.extractUserId();
+      // number links per note
+      this.linkCount = 2;
+      if(!this.userInfoParsed) {
+        this.linkCount = 1;
+      }
     }
 
     extractUserId() {
@@ -62,8 +71,8 @@
     }
 
     addHelperLinks() {
-      const articleContainers = document.querySelectorAll('#qa-NOTES_SIDEBAR .rv-sticky-leaf-node');
-      helper.log('article containers %s', articleContainers);
+      const articleContainers = document.querySelectorAll(this.articleContainerSelector);
+      helper.log('article containers %s', articleContainers.length);
       for (let articleContainer of articleContainers) {
         this.addHelperLinkToArticleContainer(articleContainer);
       }
@@ -82,28 +91,91 @@
     }
 
     addHelperLinkToArticleContainer(articleContainer) {
-      if (articleContainer.hasAttribute(this.attrName)) {
-        // already annotated
+      const attrVal = articleContainer.getAttribute(this.attrName);
+      if (attrVal) {
+        // already annotated, it is possible we lost the links due to evernote ui rendering, rerender it
+        // no need to rerender for "null" attrs
+        if(attrVal === this.attrValueNull) {
+          return;
+        }
+        this.tryRerenderLinks(articleContainer, attrVal);
         return;
       }
 
+      const noteId = this.extractNoteId(articleContainer);
+      if (!noteId) {
+        this.log('skip render links for container because there is no note id');
+        articleContainer.setAttribute(this.attrName, this.attrValueNull);
+        return;
+      }
+      if(!this.tryInsertLinks(articleContainer, noteId)) {
+        this.log('failed to insert links for %s', noteId);
+        articleContainer.setAttribute(this.attrName, this.attrValueNull);
+        return;
+      }
+      this.log('added links for %s', noteId);
+      articleContainer.setAttribute(this.attrName, noteId);
+    }
+
+    extractNoteId(articleContainer) {
       const articleNode = this.selectChildTag(articleContainer, 'article', 0);
       if (articleNode && articleNode.id) {
         const split = articleNode.id.split('_');
         const noteId = split[0];
-        helper.log('note id %s', noteId);
-        // find title
-        const titleEle = this.findTitleEle(articleNode);
-        if (titleEle) {
-          this.insertLinks(titleEle.parentElement, noteId);
-          articleContainer.setAttribute(this.attrName, noteId);
-          return;
-        }
-        articleContainer.setAttribute(this.attrName, this.attrValueNull);
-        return;
+        helper.log('extracted note id %s', noteId);
+        return noteId;
       }
 
-      articleContainer.setAttribute(this.attrName, this.attrValueNull);
+      return null;
+    }
+
+    tryRerenderLinks(articleContainer, noteId){
+      const links = articleContainer.querySelectorAll(`[${this.linkAttrName}]`);
+      // same amount of links, return
+      if(links.length === this.linkCount) {
+        return;
+      }
+      this.log('need to rerender for %s', noteId);
+      // remove old links
+      for (const link of links) {
+        link.remove();
+      }
+      this.tryInsertLinks(articleContainer, noteId);
+    }
+
+    tryInsertLinks(articleContainer, noteId) {
+      const linkInsertElement = this.findLinkInsertionElement(articleContainer);
+      if(!linkInsertElement) {
+        this.log('failed to find insertion point for %s', noteId);
+        return false;
+      }
+      this.insertLinks(linkInsertElement, noteId);
+      return true;
+    }
+
+    findLinkInsertionElement(articleContainer) {
+      const articleNode = this.selectChildTag(articleContainer, 'article', 0);
+      if(!articleNode) {
+        return null;
+      }
+
+      const titleEle = this.findTitleEle(articleNode);
+      if (!titleEle) {
+        return null;
+      }
+      return titleEle.parentElement;
+    }
+
+    findTitleEle(ele) {
+      // find the span without div child, that is where the title is
+      const spans = ele.querySelectorAll('span');
+      for (const spanEle of spans) {
+        const div = this.selectChildTag(spanEle, 'div', 0);
+        if (!div) {
+          return spanEle;
+        }
+      }
+      return null;
     }
 
     insertLinks(ele, noteId) {
@@ -139,17 +211,6 @@
       node.innerHTML = '<svg width="20" height="20" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M6.25 4.75a1.5 1.5 0 0 0-1.5 1.5v11.5a1.5 1.5 0 0 0 1.5 1.5h11.5a1.5 1.5 0 0 0 1.5-1.5v-4a1 1 0 1 1 2 0v4a3.5 3.5 0 0 1-3.5 3.5H6.25a3.5 3.5 0 0 1-3.5-3.5V6.25a3.5 3.5 0 0 1 3.5-3.5h4a1 1 0 1 1 0 2h-4Zm6.5-1a1 1 0 0 1 1-1h6.5a1 1 0 0 1 1 1v6.5a1 1 0 1 1-2 0V6.164l-4.793 4.793a1 1 0 1 1-1.414-1.414l4.793-4.793H13.75a1 1 0 0 1-1-1Z" fill="#212121"/></svg>';
       node.title = "open note in a new tab";
       return node;
-    }
-
-    findTitleEle(ele) {
-      const spans = ele.querySelectorAll('span');
-      for (const spanEle of spans) {
-        const div = this.selectChildTag(spanEle, 'div', 0);
-        if (!div) {
-          return spanEle;
-        }
-      }
-      return null;
     }
 
     selectChildTag(parentNode, tagName, index) {
